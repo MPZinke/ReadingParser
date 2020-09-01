12923210ec30f16fbb79d5bc5c46229e2365df41
mpzinke <31944952+MPZinke@users.noreply.github.com>
2020-09-01 10:29:12 -0500

    Updates correct files for directory checking & output directory

--- src/Scrubber.java
+++ src/Scrubber.java
@@ -1,14 +1,37 @@
 
+/***********************************************************************************************************
+*
+*	created by: MPZinke for St. Peter's Orthodox Church [Fort Worth, TX]
+*	on 2020.08.06
+*
+*	DESCRIPTION:	Scrapes Bible readings from month Orthodox reading announcment. 
+*						The purpose is to assist Annie Fischer in the operation of her job & 
+*						save time in the monthly tediousness of this task.  May His grace be 
+*						with us all.
+*	BUGS:		-Does not check that inputs have been applied.
+*	FUTURE:	-Better GUI (not for me though, I don't like UIs).
+*				-User specified scrapping formats.
+*	LICENSE:	Anyone is free to use and modify this, so long as it is within the confines of the
+*				United States law & not used maliciously.  I do not assume liability for any 
+*				outcomes of usage, nor do I maintain responsibility for usage, upkeep or bug
+*				fixing.
+*
+***********************************************************************************************************/
 
-import java.util.List;
-import java.util.ArrayList;
+
+import java.io.FileWriter;
 import java.io.FileInputStream;
+import java.io.IOException;
+
 import java.lang.System;
 // package
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 
 
+// main functional class that scrapes information from word document.
+// opens file, pulls information, divides info into Day objects (for each day).
+// data is then written into a text file when ::to_file() is called.
 public class Scrubber
 {
 	int[] MONTH_LENGTH = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -22,6 +45,11 @@ public class Scrubber
 	String _file_contents;
 	Day[] _days;
 
+
+	// CONSTRUCTOR
+
+	// takes the filename of the file to open & parse, month number (# of days in month), year(if leap year).
+	// reads document, parses data into Day objects.
 	Scrubber(String filename, int month, int year) throws Exception
 	{
 		_filename = filename;
@@ -29,7 +57,7 @@ public class Scrubber
 		_year = year;
 
 		// check for leap year
-		if(_month == 2 && _year % 4 == 0) _month_length = 29;
+		if(_month == 1 && _year % 4 == 0) _month_length = 29;
 		else _month_length = MONTH_LENGTH[_month];
 
 		read_document();
@@ -37,6 +65,8 @@ public class Scrubber
 	}
 
 
+	// pull all data from document based on filename.
+	// only accepts OOXML documents for now.
 	public void read_document() throws Exception
 	{
 		FileInputStream file = new FileInputStream(_filename);
@@ -46,6 +76,9 @@ public class Scrubber
 	}
 
 
+	// go through each day of month & find the day in the file procedurally line by line.
+	// create Day object when day found from context determined in ::line_is_start_of_day(..).
+	// adds object to array.
 	public void parse_days() throws Exception
 	{
 		String lines[] = _file_contents.split("\n");
@@ -56,13 +89,17 @@ public class Scrubber
 			// iterate 
 			while(line_number < lines.length && !line_is_start_of_day(day, lines[line_number])) line_number++;
 
-			_days[day-1] = new Day(day, lines, line_number);
-			_days[day-1].find_readings();
+			_days[day-1] = new Day(day, _month, _year, lines, line_number);
 		}
 	}
 
 
 	// UTILITY
+
+	// checks whether the current line is start of the day. used to determine where day's info ends.
+	// takes the next day's value (day of month), the line to be checked.
+	// looks if the first word is the day's value passed & the next word is a day of week that has been bracketed.
+	// returns the bool condition of this.
 	public Boolean line_is_start_of_day(int day, String line)
 	{
 		String words[] = line.split(" ");
@@ -70,6 +107,9 @@ public class Scrubber
 	}
 
 
+	// check if the word passed is a day of the week surrounded by brackets.
+	// takes the word that is to be checked.
+	// returns whether the word is any of the days of week.
 	public Boolean word_is_bracketed_day_of_week(String word)
 	{
 		// I know that the hardcoding is hideous, but the absence of branching saves cycles (you can still read it) :)
@@ -79,11 +119,23 @@ public class Scrubber
 	}
 
 
-	public String to_string() throws Exception
+	// returns converted parsed data into string format that Annie uses (more or less).
+	@Override
+	public String toString()
 	{
 		String days_string = new String();
-		for(int x = 0; x < _days.length; x++) days_string += _days[x].toString() + "\n";
+		for(int x = 0; x < _days.length; x++) days_string += _days[x].toString()+"\n";
 
 		return days_string;
+	}
+
+
+	// creates a text file containing the desired data determined in ::toString() -> Day::toString().
+	public void to_file(String path) throws IOException
+	{
+		String filename =	path+"/Readings_"+Integer.toString(_month+1)+"_"+Integer.toString(_year)+".txt";
+		FileWriter writer = new FileWriter(filename);
+		writer.write(this.toString());
+		writer.close();
 	}
 }
--- src/WindowFrame.java
+++ src/WindowFrame.java
@@ -1,11 +1,29 @@
 
+/***********************************************************************************************************
+*
+*	created by: MPZinke for St. Peter's Orthodox Church [Fort Worth, TX]
+*	on 2020.08.06
+*
+*	DESCRIPTION:	Scrapes Bible readings from month Orthodox reading announcment. 
+*						The purpose is to assist Annie Fischer in the operation of her job & 
+*						save time in the monthly tediousness of this task.  May His grace be 
+*						with us all.
+*	BUGS:		-Does not check that inputs have been applied.
+*	FUTURE:	-Better GUI (not for me though, I don't like UIs).
+*				-User specified scrapping formats.
+*	LICENSE:	Anyone is free to use and modify this, so long as it is within the confines of the
+*				United States law & not used maliciously.  I do not assume liability for any 
+*				outcomes of usage, nor do I maintain responsibility for usage, upkeep or bug
+*				fixing.
+*
+***********************************************************************************************************/
+
+
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.event.WindowStateListener;
-import java.awt.event.WindowEvent;
 
-import javax.swing.BoxLayout;
+import javax.swing.BorderFactory;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser. FileNameExtensionFilter ;
 import javax.swing.JFileChooser;
@@ -13,64 +31,87 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JLabel;
-import javax.swing.UIManager;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 import java.io.File;
 
+
+// main GUI class.
+// takes user input for selecting file, determining days in month, calling Scrubber class.
 public class WindowFrame extends JFrame
 {
 	String[] MONTHS =	{
 								"January", "February", "March", "April", "May", "June",
 								"July", "August", "September",  "October",  "November",  "December"
-							};
+							};  // used as selection for determing length of month
 
 	// GUI
 	private JPanel _panel;
 	private JPanel _button_panel;
 	private JPanel _output_panel;
 
-	private JButton _browse_button;
-	private JFileChooser _file_chooser;
+	private JButton _input_browse_button;
+	private JButton _output_browse_button;
 	private JButton _parse_button;
 
 	private JComboBox<String> _month_combobox;
 	private JComboBox<Integer> _year_combobox;
 
 	// USER DATA
-	private String _filepath;
+	private String _input_filepath = null;
+	private String _output_folderpath = null;
 	private int _month_index;
 	private int _year;
 
 
+	// creates window object & sets up functionality.
 	WindowFrame()
 	{
 		super("Readings Scrubber");
 
-		// setup
+		// WINDOW
 		_panel = new JPanel(new GridLayout(2, 1));
-		_button_panel = new JPanel(new GridLayout(1, 4));
-		_output_panel = new JPanel();
+		_button_panel = new JPanel(new GridLayout(1, 3));
+		_output_panel = new JPanel(new GridLayout(1,1));
 
-		// BROWSE
-		_browse_button = new JButton("Select File");
-		_browse_button.setPreferredSize(new Dimension(150, 50));
-		_button_panel.add(_browse_button);
-		_browse_button.addActionListener(new ActionListener()
+		// BROWSE INPUT FILES
+		_input_browse_button = new JButton("Select Readings File");
+		_input_browse_button.setPreferredSize(new Dimension(150, 50));
+		_input_browse_button.setBackground(Properties.Purple);
+		_input_browse_button.setForeground(Properties.Orange);
+		_input_browse_button.setBorder(BorderFactory.createBevelBorder(0));
+		_button_panel.add(_input_browse_button);
+		_input_browse_button.addActionListener(new ActionListener()
 		{
 			@Override
-			public void actionPerformed(ActionEvent e) {select_file();}
+			public void actionPerformed(ActionEvent e) {select_input_file();}
+		});
+
+		// BROWSE INPUT FOLDER
+		_output_browse_button = new JButton("Select Output Folder");
+		_output_browse_button.setPreferredSize(new Dimension(150, 50));
+		_output_browse_button.setBackground(Properties.Purple);
+		_output_browse_button.setForeground(Properties.Orange);
+		_output_browse_button.setBorder(BorderFactory.createBevelBorder(0));
+		_button_panel.add(_output_browse_button);
+		_output_browse_button.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e) {select_output_file();}
 		});
 
 		// MONTH
 		_month_combobox = new JComboBox<String>(MONTHS);
+		_month_combobox.setBackground(Properties.Purple);
+		_month_combobox.setForeground(Properties.Orange);
+		_month_combobox.setBorder(BorderFactory.createBevelBorder(0));
+		_month_index = 0;  // HARDCODED
 		_month_combobox.addActionListener(new ActionListener()
 		{
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				JComboBox box = (JComboBox)e.getSource();
+				JComboBox<String> box = (JComboBox<String>)e.getSource();
 				String month = (String)box.getSelectedItem();
 				for(int x = 0; x < 12; x++) if(MONTHS[x].equals(month)) _month_index = x;
 			}
@@ -78,31 +119,38 @@ public class WindowFrame extends JFrame
 		_button_panel.add(_month_combobox);
 
 		// YEAR
-		Integer[] years = {2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030};
+		Integer[] years = {2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030};  // HARDCODED
 		_year_combobox = new JComboBox<Integer>(years);
+		_year_combobox.setBackground(Properties.Purple);
+		_year_combobox.setForeground(Properties.Orange);
+		_year_combobox.setBorder(BorderFactory.createBevelBorder(0));
+		_year = 2020;  // HARDCODED
 		_year_combobox.addActionListener(new ActionListener()
 		{
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				JComboBox box = (JComboBox)e.getSource();
-				_year = (Integer)box.getSelectedItem();
+				JComboBox<Integer> box = (JComboBox<Integer>)e.getSource();
+				_year = years[(Integer)box.getSelectedItem()];
 			}
 		});
 		_button_panel.add(_year_combobox);
 
-
 		// PARSE
-		_parse_button = new JButton("Find Readings");
+		_parse_button = new JButton("Please Select File & Date First");
 		_parse_button.setPreferredSize(new Dimension(150, 50));
-		_button_panel.add(_parse_button);
+		_parse_button.setBackground(Properties.Purple);
+		_parse_button.setForeground(Properties.Orange);
+		_parse_button.setBorder(BorderFactory.createBevelBorder(0));
+		_parse_button.setEnabled(false);
+		_output_panel.add(_parse_button);
 		_parse_button.addActionListener(new ActionListener()
 		{
 			@Override
 			public void actionPerformed(ActionEvent e) {scrub_file();}
 		});
 
-		// add to GUI
+		// SETUP
 		_panel.add(_button_panel);
 		_panel.add(_output_panel);
 		this.add(_panel);
@@ -115,7 +163,9 @@ public class WindowFrame extends JFrame
 	}
 
 
-	private void select_file()
+	// creates a JFileChooser to browse files on computer.
+	// called by _input_browse_button ActionEvent. sets ::_input_filepath from selected file.
+	private void select_input_file()
 	{
 		JFileChooser file_chooser = new JFileChooser();
 		file_chooser.setCurrentDirectory(new File(System.getProperty("user.home")));
@@ -123,17 +173,42 @@ public class WindowFrame extends JFrame
 		file_chooser.addChoosableFileFilter(filter);
 
 		if(file_chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION)
-			_filepath = file_chooser.getSelectedFile().getAbsolutePath();
+		{
+			_input_filepath = file_chooser.getSelectedFile().getAbsolutePath();
+			_parse_button.setText("Find Readings for "+_input_filepath);
+		}
+
+		if(_input_filepath != null && _output_folderpath != null) _parse_button.setEnabled(true);
 	}
 
 
+	private void select_output_file()
+	{
+		JFileChooser file_chooser = new JFileChooser();
+		file_chooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+		file_chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		FileFilter filter = new FileNameExtensionFilter("Office Open XML Document", "docx");
+		file_chooser.addChoosableFileFilter(filter);
+
+		if(file_chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION)
+		{
+			_output_folderpath = file_chooser.getSelectedFile().getAbsolutePath();
+			_parse_button.setText("Find Readings for "+_output_folderpath);
+		}
+
+		if(_input_filepath != null && _output_folderpath != null) _parse_button.setEnabled(true);
+	}
+
+
+	// creates a Scrubber object to parse the file for relevent data.
+	// writes the data to an output file.
 	private void scrub_file()
 	{
 		try
 		{
-			Scrubber scrubber = new Scrubber(_filepath, _month_index, _year);
-			String output = scrubber.to_string();
-			showMessageDialog(null, output);
+			Scrubber scrubber = new Scrubber(_input_filepath, _month_index, _year);
+			scrubber.to_file(_output_folderpath);
+			showMessageDialog(null, "Successfully created file in "+_output_folderpath);
 		}
 		catch(Exception exception)
 		{
